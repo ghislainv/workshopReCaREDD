7fe3c75922d73060bf0b6e4620e5bb33758011df
Ghislain Vieilledent <ghislain.vieilledent@cirad.fr>
2017-07-07 17:32:04 +0200

    WDPA

--- defor_Africa/scripts/data.sh
+++ defor_Africa/scripts/data.sh
@@ -14,10 +14,12 @@ cd data_raw
 
 # Variables
 continent="africa"
-country="cameroon"
-iso="CMR"
-proj="EPSG:102017" # North_Pole_Lambert_Azimuthal_Equal_Area, see http://epsg.io 
-extent="410000 165000 1325000 1500000" # xmin ymin xmax ymax
+#country="cameroon"
+#iso="CMR"
+#proj="EPSG:102022" # Africa_Albers_Equal_Area_Conic, see http://epsg.io
+#proj="+proj=aea +lat_1=20 +lat_2=-23 +lat_0=0 +lon_0=25 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs"
+proj="../scripts/proj.prj"
+extent="-1883765 -1639228 738040 1556206" # xmin ymin xmax ymax
 tiles_long="38-44" # see http://dwtkns.com/srtm/
 tiles_lat="10-15"
 resolution="250"
@@ -34,11 +36,11 @@ url="http://download.geofabrik.de/africa-latest.osm.pbf"
 wget -O country.osm.pbf $url
 osmconvert country.osm.pbf -o=country.o5m
 
-# Country borders
-osmfilter country.o5m --verbose --keep='boundary=administrative and admin_level=2' > borders.osm 
-ogr2ogr -overwrite -skipfailures -f 'ESRI Shapefile' -progress \
-        -sql "SELECT osm_id, name, admin_level FROM multipolygons WHERE admin_level='2'" \
-        -lco ENCODING=UTF-8 borders.shp borders.osm
+# # Country borders
+# osmfilter country.o5m --verbose --keep='boundary=administrative and admin_level=2' > borders.osm 
+# ogr2ogr -overwrite -skipfailures -f 'ESRI Shapefile' -progress \
+#         -sql "SELECT osm_id, name, admin_level FROM multipolygons WHERE admin_level='2'" \
+#         -lco ENCODING=UTF-8 borders.shp borders.osm
 
 # Main roads
 osmfilter country.o5m --keep='highway=motorway or highway=trunk or highway=*ary' > roads.osm
@@ -57,50 +59,59 @@ ogr2ogr -overwrite -skipfailures -f 'ESRI Shapefile' -progress \
         -lco ENCODING=UTF-8 rivers.shp rivers.osm
 
 # Rasterize after reprojection
-# borders (no reprojection needed)
-ogr2ogr -overwrite -s_srs EPSG:4326 -t_srs $proj -f 'ESRI Shapefile' \
-        -lco ENCODING=UTF-8 borders_UTM.shp borders.shp
+# # borders (no reprojection needed)
+# ogr2ogr -overwrite -s_srs EPSG:4326 -t_srs $proj -f 'ESRI Shapefile' \
+#         -lco ENCODING=UTF-8 borders_UTM.shp borders.shp
 # Extent: ogrinfo borders_UTM.shp borders_UTM | grep Extent
 # towns
 ogr2ogr -overwrite -s_srs EPSG:4326 -t_srs $proj -f 'ESRI Shapefile' \
         -lco ENCODING=UTF-8 towns_UTM.shp towns.shp
 gdal_rasterize -te $extent -tap -burn 1 \
-               -co "COMPRESS=LZW" -co "PREDICTOR=2" -ot Byte \
+               -co "COMPRESS=LZW" -co "PREDICTOR=2" -co "BIGTIFF=YES" -ot Byte \
                -a_nodata 255 \
                -tr $resolution $resolution -l towns_UTM towns_UTM.shp towns.tif
 # roads
 ogr2ogr -overwrite -s_srs EPSG:4326 -t_srs $proj -f 'ESRI Shapefile' \
         -lco ENCODING=UTF-8 roads_UTM.shp roads.shp
 gdal_rasterize -te $extent -tap -burn 1 \
-               -co "COMPRESS=LZW" -co "PREDICTOR=2" -ot Byte \
+               -co "COMPRESS=LZW" -co "PREDICTOR=2" -co "BIGTIFF=YES" -ot Byte \
                -a_nodata 255 \
                -tr $resolution $resolution -l roads_UTM roads_UTM.shp roads.tif
 # rivers
 ogr2ogr -overwrite -s_srs EPSG:4326 -t_srs $proj -f 'ESRI Shapefile' \
         -lco ENCODING=UTF-8 rivers_UTM.shp rivers.shp
 gdal_rasterize -te $extent -tap -burn 1 \
-               -co "COMPRESS=LZW" -co "PREDICTOR=2" -ot Byte \
+               -co "COMPRESS=LZW" -co "PREDICTOR=2" -co "BIGTIFF=YES" -ot Byte \
                -a_nodata 255 \
                -tr $resolution $resolution -l rivers_UTM rivers_UTM.shp rivers.tif
 
-# # Convert to kml
-# ogr2ogr -f 'KML' borders.kml borders_UTM.shp
-ogr2ogr -f 'KML' towns.kml towns_UTM.shp
-ogr2ogr -f 'KML' roads.kml roads_UTM.shp
-ogr2ogr -f 'KML' rivers.kml rivers_UTM.shp
+# Convert to kml
+# ogr2ogr -f 'KML' borders.kml borders.shp
+ogr2ogr -f 'KML' towns.kml towns.shp
+ogr2ogr -f 'KML' roads.kml roads.shp
+ogr2ogr -f 'KML' rivers.kml rivers.shp
 
 # Compute distances
-gdal_proximity.py roads.tif _dist_road.tif -co "COMPRESS=LZW" -co "PREDICTOR=2" \
+gdal_proximity.py roads.tif _dist_road.tif \
+                  -co "COMPRESS=LZW" -co "PREDICTOR=2" -co "BIGTIFF=YES" \
                   -values 1 -ot UInt32 -distunits GEO
-gdal_proximity.py towns.tif _dist_town.tif -co "COMPRESS=LZW" -co "PREDICTOR=2" \
+gdal_proximity.py towns.tif _dist_town.tif \
+                  -co "COMPRESS=LZW" -co "PREDICTOR=2" -co "BIGTIFF=YES" \
                   -values 1 -ot UInt32 -distunits GEO
-gdal_proximity.py rivers.tif _dist_river.tif -co "COMPRESS=LZW" -co "PREDICTOR=2" \
+gdal_proximity.py rivers.tif _dist_river.tif \
+                  -co "COMPRESS=LZW" -co "PREDICTOR=2" -co "BIGTIFF=YES" \
                   -values 1 -ot UInt32 -distunits GEO
 
 # Add nodata
-gdal_translate -a_nodata 4294967295 -co "COMPRESS=LZW" -co "PREDICTOR=2" _dist_road.tif dist_road.tif
-gdal_translate -a_nodata 4294967295 -co "COMPRESS=LZW" -co "PREDICTOR=2" _dist_town.tif dist_town.tif
-gdal_translate -a_nodata 4294967295 -co "COMPRESS=LZW" -co "PREDICTOR=2" _dist_river.tif dist_river.tif
+gdal_translate -a_nodata 4294967295 \
+               -co "COMPRESS=LZW" -co "PREDICTOR=2" -co "BIGTIFF=YES" \
+               _dist_road.tif dist_road.tif
+gdal_translate -a_nodata 4294967295 \
+               -co "COMPRESS=LZW" -co "PREDICTOR=2" -co "BIGTIFF=YES" \
+               _dist_town.tif dist_town.tif
+gdal_translate -a_nodata 4294967295 \
+               -co "COMPRESS=LZW" -co "PREDICTOR=2" -co "BIGTIFF=YES" \
+               _dist_river.tif dist_river.tif
 
 # ===========================
 # SRTM
@@ -125,16 +136,16 @@ gdalbuildvrt srtm.vrt */*.tif
 
 # Merge and reproject
 gdalwarp -overwrite -t_srs $proj -te $extent -r bilinear \
-         -co "COMPRESS=LZW" -co "PREDICTOR=2" \
+         -co "COMPRESS=LZW" -co "PREDICTOR=2" -co "BIGTIFF=YES" \
          -tr 90 90 srtm.vrt altitude.tif
 
 # Compute slope and aspect
-gdaldem slope altitude.tif slope_.tif -co "COMPRESS=LZW" -co "PREDICTOR=2"
-gdaldem aspect altitude.tif aspect_.tif -co "COMPRESS=LZW" -co "PREDICTOR=2"
+gdaldem slope altitude.tif slope_.tif -co "COMPRESS=LZW" -co "PREDICTOR=2" -co "BIGTIFF=YES"
+gdaldem aspect altitude.tif aspect_.tif -co "COMPRESS=LZW" -co "PREDICTOR=2" -co "BIGTIFF=YES"
 
 # Convert to Int16
-gdal_translate -ot Int16 -co "COMPRESS=LZW" -co "PREDICTOR=2" slope_.tif slope.tif
-gdal_translate -ot Int16 -co "COMPRESS=LZW" -co "PREDICTOR=2" aspect_.tif aspect.tif
+gdal_translate -ot Int16 -co "COMPRESS=LZW" -co "PREDICTOR=2" -co "BIGTIFF=YES" slope_.tif slope.tif
+gdal_translate -ot Int16 -co "COMPRESS=LZW" -co "PREDICTOR=2" -co "BIGTIFF=YES" aspect_.tif aspect.tif
 
 # ===========================
 # SAPM
@@ -145,13 +156,27 @@ echo "Protected area network from Protected Planet\n"
 # See protected planet: www.protectedplanet.net
 
 # Download from Protected Planet
-url="https://www.protectedplanet.net/downloads/WDPA_Mar2017_"$iso"?type=shapefile"
+url="http://www.wdpa.org/downloads/WDPA_July2017?type=shapefile"
 wget -O pa.zip $url
 unzip pa.zip
 
+# Iso code for Africa
+iso3c="('DZA' , 'AGO' , 'BEN' , 'BWA' , 'BFA' , 'BDI' , 'CMR' , 'CPV' , 'CAF' , 'TCD' , 'COM' , 'COD' , \
+'COG' , 'CIV' , 'DJI' , 'EGY' , 'GNQ' , 'ERI' , 'ETH' , 'GAB' , 'GMB' , 'GHA' , 'GIN' , 'GNB' , \
+'KEN' , 'LSO' , 'LBR' , 'LBY' , 'MDG' , 'MWI' , 'MLI' , 'MRT' , 'MUS' , 'MYT' , 'MAR' , 'MOZ' , \
+'NAM' , 'NER' , 'NGA' , 'REU' , 'RWA' , 'SHN' , 'STP' , 'SEN' , 'SYC' , 'SLE' , 'SOM' , 'ZAF' , \
+'SSD' , 'SDN' , 'SWZ' , 'TZA' , 'TGO' , 'TUN' , 'UGA' , 'ESH' , 'ZMB' , 'ZWE')"
+
+# Sql statement for African countries
+input_file="WDPA_July2017-shapefile-polygons.shp"
+where_statement="WHERE ISO3C IN "$iso3c
+ogr2ogr -overwrite -skipfailures -f "ESRI Shapefile" -progress \
+        -where "ISO3C IN ('BEN' , 'MDG')" \
+        -lco ENCODING=UTF-8 pa_iso.shp $input_file
+
 # Reproject
-input_file="WDPA_Mar2017_"$iso"-shapefile-polygons.shp"
 ogr2ogr -overwrite -skipfailures -f 'ESRI Shapefile' -progress \
+        -clipsrc $extent \
         -s_srs EPSG:4326 -t_srs $proj \
         -lco ENCODING=UTF-8 pa_UTM.shp $input_file
 
